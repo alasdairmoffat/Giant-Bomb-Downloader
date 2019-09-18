cb8fa28ccea1292a101079c3a5634fe6fcd15ab6
Alasdair Moffat <contact@alasdairmoffat.com>
2019-09-18 10:57:20 +0100

    Working script

--- downloader.py
+++ downloader.py
@@ -1,7 +1,18 @@
 import datetime
-import json
-import requests
-import urllib.request
+import pathlib
+import unicodedata
+
+try:
+    import requests
+except ModuleNotFoundError:
+    print("###  pip install requests  ###")
+    raise
+
+try:
+    from clint.textui import progress
+except ModuleNotFoundError:
+    print('### pip install clint ###')
+    raise
 
 directory = '<directory>'
 
@@ -11,13 +22,14 @@ with open(f'{directory}gb_most_recent', 'r') as f:
     lastPublishDate = datetime.datetime.strptime(f.read(), '%Y-%m-%d %H:%M:%S')
 
 api_key = '<API KEY>'
-url = f'https://www.giantbomb.com/api/videos/'
+url = 'https://www.giantbomb.com/api/videos/'
 params = {
     'api_key': api_key,
     'format': 'json',
     'filter': f'publish_date:{lastPublishDate + datetime.timedelta(seconds=1)}|{now}'
 }
 
+print('Retrieving Videos...')
 response = requests.get(url,
                         headers={'User-agent': 'gb_dl'},
                         params=params)
@@ -26,31 +38,42 @@ response = requests.get(url,
 videos = response.json()['results']
 videos.reverse()
 
-# data = json.loads(response)
+count = 1
+numVideos = len(videos)
 
-'''
-name is in videos[index]['name']
-download is in videos[index]['high_url']
-date is in videos[index]['publish_date']
-Bombcast has videos[index]['video_show']['title'] == 'Giant Bombcast'
-'''
+
+def correctFileName(name, extension):
+    newName = name.replace(':', ' -')
+    newName = newName.replace('/', '-')
+    newName = newName.replace('"', '\'')
+    charsToRemove = ['\\', '?', '%', '*', '|', '<', '>']
+    for char in charsToRemove:
+        newName = newName.replace(char, '')
+    return f'{newName}{extension}'
 
 
 if videos:
     for x in videos:
         if x['video_show'] is None or x['video_show']['title'] != 'Giant Bombcast':
-            videoName = x['name'].replace(':', ' -')
+            extension = pathlib.Path(x['high_url']).suffix
+            fileName = correctFileName(x['name'], extension)
             videoDate = x['publish_date']
             targetURL = f"{x['high_url']}?api_key={api_key}"
-            print(f'Downloading {videoName} : {videoDate}...')
+            print(
+                f'Downloading {count} of {numVideos} - {fileName} : {videoDate}...')
 
-            # urllib.request.urlretrieve(
-            #     targetURL, f'{directory}{videoName}.mp4')
+            r = requests.get(targetURL, stream=True)
+            path = f'{directory}{fileName}'
+            with open(path, 'wb') as f:
+                total_length = int(r.headers.get('content-length'))
+                for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+                    if chunk:
+                        f.write(chunk)
+                        f.flush()
 
-            r = requests.get(targetURL)
-            with open(f'{directory}{videoName}', 'wb') as f:
-                f.write(r.content)
-
-            print('...Download complete')
             with open(f'{directory}gb_most_recent', 'w') as f:
                 f.write(videoDate)
+
+        count += 1
+
+print('All videos downloaded')
